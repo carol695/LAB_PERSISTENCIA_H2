062c071be4526d10f22f978c911007f6e04b5295
carol <caroltatianacely@gmail.com>
2023-09-03 18:04:42 -0500

    H2 persistence lab

---
+++ src/main/java/com/example/LAB_PERSISTENCIA/LabPersistenciaApplication.java
@@ -0,0 +1,13 @@
+package com.example.LAB_PERSISTENCIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPersistenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPersistenciaApplication.class, args);
+	}
+
+}
